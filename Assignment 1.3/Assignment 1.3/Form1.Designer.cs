489faa182cd13ecf022596eb35449d173daf35dc
Anthony Parsch <anthonyparsch@gmail.com>
2018-01-14 14:53:09 -0500

    Completed Assignment 1.3

---
+++ Assignment 1.3/Assignment 1.3/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Assignment_1._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(13, 13);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_CheckKeyPressed);
+            this.txtInput.Leave += new System.EventHandler(this.txtInput_Leave);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(13, 40);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(259, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 77);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Assignment 1.3/Assignment 1.3/Form1.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Anthony Parsch
+ * January 14, 2018
+ * Assignment 1.3: Text Boxes
+ * 
+ * Custom Features:
+ * Output Textbox is read only
+ * Program updates on exit of the input textbox as well as when enter is pressed
+ * Form title updates when output textbox is updated
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1._3
+{
+    public partial class Form1 : Form
+    {
+        private const string outputStart = "Hello, ";
+        private const string outputEnd = "! Welcome to C#!!";
+        private const string titleStart = "Hello ";
+        private const string titleEnd = "!";
+        private const string titleDefault = "Hello User!";
+        private string userName = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Updates the form's title.
+        /// </summary>
+        private void UpdateTitle()
+        {
+            if (userName != null)
+            {
+                Text = titleStart + userName + titleEnd;
+            }
+            else
+            {
+                Text = titleDefault;
+            }
+        }
+
+        /// <summary>
+        /// Update the output textbox.
+        /// </summary>
+        private void UpdateOutputTextbox()
+        {
+            if (userName != null)
+            {
+                txtOutput.Text = outputStart + userName + outputEnd;
+            }
+            else
+            {
+                txtOutput.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Updates the username.
+        /// </summary>
+        private void UpdateUserName()
+        {
+            UpdateUserName(txtInput);
+        }
+
+        /// <summary>
+        /// Updates the username based off of the given textbox.
+        /// </summary>
+        /// <param name="textBox">The textbox containing the username.</param>
+        private void UpdateUserName(TextBox textBox)
+        {
+            userName = textBox.Text;
+        }
+
+        /// <summary>
+        /// Updates the username, title, and ouput textbox.
+        /// </summary>
+        private void FullUpdate()
+        {
+            UpdateUserName();
+            UpdateTitle();
+            UpdateOutputTextbox();
+        }
+
+        /// <summary>
+        /// Update the title at the start of the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateTitle();
+        }
+
+        /// <summary>
+        /// When we leave the input textbox, run a full update.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtInput_Leave(object sender, EventArgs e)
+        {
+            FullUpdate();
+        }
+
+        /// <summary>
+        /// When a user presses enter inside of the input textbox, run a full update.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtInput_CheckKeyPressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                FullUpdate();
+            }
+        }
+    }
+}
